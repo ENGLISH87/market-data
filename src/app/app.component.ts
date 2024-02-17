2bdccbef8c94af73f61dc009a2fb0e65bdc88f95
Shaun English <shaun_english@hotmail.co.uk>
2024-02-17 13:17:44 +0000

    feat: add route change animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { RouterOutlet } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { filter } from 'rxjs';
+import { routeStateTrigger } from './core/animations/animations';
 import { SidebarComponent } from './core/components/sidebar/sidebar.component';
 import { subscribeToPriceEventsFactory } from './state/market-data/market-data.actions';
 import { apiConnect } from './state/ui-settings/ui-settings.actions';
@@ -13,6 +14,7 @@ import { selectIsConnected } from './state/ui-settings/ui-settings.selectors';
   standalone: true,
   imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
+  animations: [routeStateTrigger],
 })
 export class AppComponent {
   constructor(private store: Store) {
